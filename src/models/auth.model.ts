5a3e7bfb6dc3f5f07efcc5e64057093091831f22
kofta999 <mostafaxxx555@gmail.com>
2023-09-20 16:11:29 +0300

    added better validation

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -1,18 +1,31 @@
 import { Elysia, t } from "elysia";
 
+const passwordSchema = t.String({
+  minLength: 8,
+  pattern: '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)',
+  default: "Minimum 8 characters, at least one uppercase letter, one lowercase letter, and one digit"
+});
+
+const emailSchema = t.String({
+  format: "email",
+  default: "Email format such as example@example.com"
+})
+
 export const authModel = new Elysia().model({
   signup: t.Object({
-    name: t.String(),
-    email: t.String(),
-    password: t.String(),
+    name: t.String({ minLength: 3 }),
+    email: emailSchema,
+    password: passwordSchema,
   }),
   login: t.Object({
-    email: t.String(),
-    password: t.String(),
+    email: emailSchema,
+    password: passwordSchema,
   }),
   resetPassword: t.Object({
-    email: t.String(),
+    email: emailSchema,
   }),
-  newPassword: t.Object({ password: t.String(), userId: t.String() }),
-  
+  newPassword: t.Object({
+    password: passwordSchema,
+    userId: t.String()
+  }),
 });
